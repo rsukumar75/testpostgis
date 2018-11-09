be73f030c630bfd85be657653b65b86f2848a47f
rsukumar75 <rish.sukumar@gmail.com>
2018-11-08 18:18:29 -0800

    Added Age Field

--- lex/models.py
+++ lex/models.py
@@ -3,7 +3,7 @@ from django.contrib.gis.db import models
 class User(models.Model):
 	name = models.TextField()
 	location = models.PointField(geography=True, null=True, blank=False)
-	
+	age = models.IntegerField()
 	def __str__(self):
 		return self.name
 	
